9314c59d92ce50b27e390b5415ace2baf14e8d9e
Haris Ahmad <krazzyharis@gmail.com>
2023-07-08 00:10:19 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -20,8 +20,12 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
 
+# extra comments
+
+
+
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument('--config', default='params.yaml')
+    args.add_argument('--config', default ='params.yaml')
     parsed_args = args.parse_args()
     get_data(config_path=parsed_args.config)
